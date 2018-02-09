a9f77c5d4cfa0c47d56f01c032cc89952517c7ed
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-09 13:56:45 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express"),bodyParser=require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/', function(request, response){
+  console.log(request.body);      // your JSON
+   response.send(request.body);    // echo the result back
+});
+
+app.listen("3300", function(){
+  console.log('Server up');
+});
