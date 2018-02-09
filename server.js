c83a994772bcec3b167d0f192b3224d866412cb2
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-09 15:03:50 -0500

    capa model

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express"),bodyParser=require("body-parser");
+var Reporte = require("./model/reporte");
 
 var app = express();
 app.use(bodyParser.json());
 
 app.post('/', function(request, response){
-  console.log(request.body);      // your JSON
-   response.send(request.body);    // echo the result back
+  report = new Reporte(request.body);
+  response.send(report.getAtt());    // echo the result back
 });
 
 app.listen("3300", function(){
