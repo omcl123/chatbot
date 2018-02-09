7af7cdc7f6b3bc11f4e1fba020b3179c0b0a07d8
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-09 15:05:39 -0500

    capa modelo

---
+++ model/reporte.js
@@ -0,0 +1,13 @@
+
+function Reporte(str){
+	this._clientId=str["clientID"];
+	console.log(this._clientId);
+	this._type=JSON.stringify(str["type"]);
+	
+};
+
+Reporte.prototype.getAtt=function (){
+	return("{'reporte': "+this._type+",'ID':"+this._clientId+" }");
+}
+
+module.exports=Reporte;
