590e6df5f32e110efbb6e316e2e82931658bd333
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-10 08:38:43 -0500

    devuelve url

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require("express"),bodyParser=require("body-parser");
-var Reporte = require("./model/reporte");
+var Processer = require("./processer/processer");
 
 var app = express();
 app.use(bodyParser.json());
 
 app.post('/', function(request, response){
-  report = new Reporte(request.body);
-  response.send(report.getAtt());    // echo the result back
+	var url =Processer.devuelveUrl(request.body);
+	response.send(url);    // echo the result back
 });
 
 app.listen("3300", function(){
   console.log('Server up');
 });
+
