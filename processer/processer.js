ed0fe1a679e2c62b96197d7ad765988a9fb25279
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-11 18:37:53 -0500

    devuelve una ayuda basica

--- processer/processer.js
+++ processer/processer.js
@@ -3,9 +3,16 @@ var generador= require("../reporter/genReporte");
 var Reporte = require("../model/reporte");
 
 module.exports={
+
 	devuelveUrl: function(str){
-		report = new Reporte(str);
-		//console.log("json="+str);
-		return generador.generateReport(report);
+		if(str["type"].localeCompare("help")==0){
+			return "Puede pedir 3 tipos de reportes: bar-chart, pie-chart y line-chart";
+		}else{
+		    report = new Reporte(str);
+			//console.log("json="+str);
+			return generador.generateReport(report);		
+		}
+		
 	}
+	
 }
