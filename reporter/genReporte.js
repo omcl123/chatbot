09c2b80abcd5b26550c98c47837e465538348c17
negar14 <inoku_0099@hotmail.com>
2018-02-11 09:33:16 -0500

    graficos hardcodeados

--- reporter/genReporte.js
+++ reporter/genReporte.js
@@ -1,8 +1,5 @@
 var fs = require('fs');
-function buildHtml(tipo) {
-  var header = '';
-  var body = "<h1>este es un grafico "+ tipo+"</h1>";
-
+function buildHtml(header,body) {
   return '<!DOCTYPE html>' + '<html><header>' + header + '</header><body>' + body + '</body></html>';
 };
 
@@ -15,31 +12,142 @@ module.exports={
     var dirName ='./'+idCliente;
     //console.log(dirName);
     var dirAndPage = dirName+"/"+idPage+".html";
+    var header='';
+    var body = '';
 
     if (fs.existsSync(dirName)) {
-      console.log("existe carpeta cliente");
+      console.log("existe carpeta cliente " + idCliente);
     }else{
-      console.log("NO existe carpeta cliente");
+      console.log("No existe carpeta cliente "+idCliente);
       fs.mkdirSync(dirName)
-      //console.log("carpeta creada");
+      console.log("carpeta "+idCliente+" creada");
     }
-    console.log(objData._type);
-    switch(objData._type){
-      case "bar-chart":
-        console.log("pidio-bar-char");
-        break;
-      case "pie-chart":
-        console.log("pidio-pie-char");
-        break;
-      case "line-chart":
-        console.log("pidio-line-chart");
-        break;
+    var typeRep = objData._type;
+    typeRep = typeRep.replace(/"/g,'');
+
+    if(typeRep=="bar-chart"){
+      //Bar
+      header = '<meta charset="UTF-8">'+
+                '<script>'+
+                'window.onload = function () {'+
+                'var chart = new CanvasJS.Chart("chartContainer", {'+
+                  'animationEnabled: true,'+
+                  'title:{'+
+                    'text:"Fortune 500 Companies by Country"'+
+                  '},'+
+                  'axisX:{'+
+                    'interval: 1'+
+                  '},'+
+                  'axisY2:{'+
+                    'interlacedColor: "rgba(1,77,101,.2)",'+
+                    'gridColor: "rgba(1,77,101,.1)",'+
+                    'title: "Number of Companies"'+
+                  '},'+
+                  'data: [{'+
+                    'type: "bar",'+
+                    'name: "companies",'+
+                    'axisYType: "secondary",'+
+                    'color: "#014D65",'+
+                    'dataPoints: ['+
+                      '{ y: 3, label: "Sweden" },'+
+                      '{ y: 7, label: "Taiwan" },'+
+                      '{ y: 5, label: "Russia" },'+
+                      '{ y: 9, label: "Spain" },'+
+                      '{ y: 7, label: "Brazil" },'+
+                      '{ y: 7, label: "India" },'+
+                      '{ y: 9, label: "Italy" },'+
+                      '{ y: 8, label: "Australia" },'+
+                      '{ y: 11, label: "Canada" },'+
+                      '{ y: 15, label: "South Korea" },'+
+                      '{ y: 12, label: "Netherlands" },'+
+                      '{ y: 15, label: "Switzerland" },'+
+                      '{ y: 25, label: "Britain" },'+
+                      '{ y: 28, label: "Germany" },'+
+                      '{ y: 29, label: "France" },'+
+                      '{ y: 52, label: "Japan" },'+
+                      '{ y: 103, label: "China" },'+
+                      '{ y: 134, label: "US" }'+
+                    ']'+
+                  '}]'+
+                '});'+
+                'chart.render();'+
+                '}'+
+                '</script>';
+      body = '<div id="chartContainer" style="height: 370px; max-width: 920px; margin: 0px auto;"></div>'+
+              '<script src="../canvasjs.min.js"></script>';
+    }else if(typeRep=="pie-chart"){
+      //Pie
+      header = '<meta charset="UTF-8">'+
+                '<script>'+
+                'window.onload = function() {'+
+                'var chart = new CanvasJS.Chart("chartContainer", {'+
+                  'animationEnabled: true,'+
+                  'title: {'+
+                    'text: "Desktop Search Engine Market Share - 2016"'+
+                  '},'+
+                  'data: [{'+
+                    'type: "pie",'+
+                    'startAngle: 240,'+
+                    'yValueFormatString: "##0.00\'%\'",'+
+                    'indexLabel: "{label} {y}",'+
+                    'dataPoints: ['+
+                      '{y: 79.45, label: "Google"},'+
+                      '{y: 7.31, label: "Bing"},'+
+                      '{y: 7.06, label: "Baidu"},'+
+                      '{y: 4.91, label: "Yahoo"},'+
+                      '{y: 1.26, label: "Others"}'+
+                    ']'+
+                  '}]'+
+                '});'+
+                'chart.render();'+
+                '}'+
+                '</script>';
+      body=  '<div id="chartContainer" style="height: 370px; max-width: 920px; margin: 0px auto;"></div>'+
+              '<script src="../canvasjs.min.js"></script>';
+    }else if(typeRep=="line-chart"){
+      //Line
+      header='<meta charset="UTF-8">'+
+              '<script>'+
+              'window.onload = function () {'+
+              'var chart = new CanvasJS.Chart("chartContainer", {'+
+                'animationEnabled: true,'+
+                'theme: "light2",'+
+                'title:{'+
+                  'text: "Simple Line Chart"'+
+                '},'+
+                'axisY:{'+
+                  'includeZero: false'+
+                '},'+
+                'data: [{'+        
+                  'type: "line",'+       
+                  'dataPoints: ['+
+                    '{ y: 450 },'+
+                    '{ y: 414},'+
+                    '{ y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },'+
+                    '{ y: 460 },'+
+                    '{ y: 450 },'+
+                    '{ y: 500 },'+
+                    '{ y: 480 },'+
+                    '{ y: 480 },'+
+                    '{ y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },'+
+                    '{ y: 500 },'+
+                    '{ y: 480 },'+
+                    '{ y: 510 }'+
+                  ']'+
+                '}]'+
+              '});'+
+              'chart.render();'+
+              '}'+
+              '</script>';
+      body = '<div id="chartContainer" style="height: 370px; max-width: 920px; margin: 0px auto;"></div>'+
+              '<script src="../canvasjs.min.js"></script>';
+    }else{
+      return "no existe ese reporte";
     }
-    console.log("Pasa el switch");
 
     var stream = fs.createWriteStream(dirAndPage);
     stream.once('open', function(fd) {
-      var html = buildHtml(objData._type);
+      var html = buildHtml(header,body);
       stream.end(html);
     });
 
