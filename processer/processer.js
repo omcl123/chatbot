62cc194766b71f6185192125d3fd31d7a2d47c7b
omcl1302 <a20121302@pucp.pe>
2018-02-15 09:39:50 -0500

    mejora modelo

--- processer/processer.js
+++ processer/processer.js
@@ -1,6 +1,6 @@
 
 var generador= require("../reporter/genReporte");
-//var Reporte = require("../model/reporte");
+var Reporte = require("../model/reporte");
 
 module.exports={
 
@@ -9,7 +9,7 @@ module.exports={
 		if(str["type1"]=="help"){
 			return "Puede pedir 3 tipos de reportes: bar-chart, pie-chart y line-chart";
 		}else{
-		    //report = new Reporte(str);
+		    report = new Reporte(str);
 			//Con los datos de json en str, se hacen las consultas y se forma un nuevo json que se enviará al reporteador.
 
 			var jsonGenerado = JSON.parse('{"clientID":12345678,"title":"titulo para el bar","type":"bar-chart","legendX":"","legendY":"leyenda de Y","data":[{"label":"columna 1","value":40,"clickable":true,"title":"titulo de pizza 1","type":"pie-chart","legendX":"","legendY":"","data":[{"label":"pizza 1","value":44},{"label":"pizza 2","value":37}]},{"label":"columna 2","value":80,"clickable":true,"title":"titulo de pizza 2","type":"pie-chart","legendX":"","legendY":"","data":[{"label":"pizza 1.1","value":16},{"label":"pizza 2.2","value":28}]}]}');
