8b94ce15ed924b8d3eff868d4047bd42b4e5b0d4
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-16 15:17:27 -0500

    primer nivel

--- processer/processer.js
+++ processer/processer.js
@@ -8,9 +8,12 @@ function devuelveUrl(str){
 	Reporte.principal(str,repJson).then(function(){
 		console.log("entro");
 		console.log(repJson);
+		repJson=JSON.stringify(repJson);
+		repJson=JSON.parse(repJson);
+		return generador.generateReport(repJson);	
 	});
-	var jsonGenerado = JSON.parse('{"clientID":12345678,"title":"titulo para el pie","type":"pie-chart","legendX":"","legendY":"","data":[{"label":"pizza 1","value":40,"clickable":true,"title":"titulo del line - pizza 1","type":"line-chart","legendX":"","legendY":"","data":[{"label":"linea 1","value":0,"clickable":false,"title":"","type":"","legendX":"","legendY":"","data":[{"label":"fecha 1","value":44},{"label":"fecha 2","value":37}]}]},{"label":"pizza 2","value":80,"clickable":true,"title":"titulo del line - pizza 2","type":"line-chart","legendX":"","legendY":"","data":[{"label":"linea 2","value":0,"clickable":false,"title":"","type":"","legendX":"","legendY":"","data":[{"label":"fecha 1","value":16},{"label":"fecha 2","value":28}]}]}]}');
-	return generador.generateReport(jsonGenerado);	
+	//var jsonGenerado = JSON.parse('{"clientID":12345678,"title":"titulo para el pie","type":"pie-chart","legendX":"","legendY":"","data":[{"label":"pizza 1","value":40,"clickable":true,"title":"titulo del line - pizza 1","type":"line-chart","legendX":"","legendY":"","data":[{"label":"linea 1","value":0,"clickable":false,"title":"","type":"","legendX":"","legendY":"","data":[{"label":"fecha 1","value":44},{"label":"fecha 2","value":37}]}]},{"label":"pizza 2","value":80,"clickable":true,"title":"titulo del line - pizza 2","type":"line-chart","legendX":"","legendY":"","data":[{"label":"linea 2","value":0,"clickable":false,"title":"","type":"","legendX":"","legendY":"","data":[{"label":"fecha 1","value":16},{"label":"fecha 2","value":28}]}]}]}');
+	
 }
 
 
