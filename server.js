df02230689ed0c14bc9d056dbf5838460d32f974
Oscar <Oscar@DESKTOP-44C66HF>
2018-02-16 15:51:42 -0500

    url primer nivel

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/', function(request, response){
-	var url =Processer.devuelveUrl(request.body);
-	response.send(url);    // echo the result back
+		var url;
+		Processer.devuelveUrl(request.body,url).then(function(url){
+		//console.log("en server");
+		//console.log(url);
+		return response.send(url);
+	});
+	
 });
 
 app.listen("3300", function(){
